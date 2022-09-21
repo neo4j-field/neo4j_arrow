76cff6aed4ce5b6984337e1db42f8426f21f6e58
Dave Voutila <voutilad@gmail.com>
2022-09-21 09:43:37 -0400

    initial import from the dataflow flex template project

---
+++ neo4j_arrow/__init__.py
@@ -0,0 +1,5 @@
+__all__ = [ "Neo4jArrowClient", "model" ]
+
+from ._client import Neo4jArrowClient
+
+from . import model
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="neo4j_arrow",
+    version="0.1.0",
+
+    url="https://github.com/neo4j-field/neo4j_arrow",
+    maintainer="Dave Voutila",
+    maintainer_email="dave.voutila@neotechnology.com",
+    license="Apache License 2.0",
+
+    install_requires=[
+        "pyarrow=9",
+    ],
+    packages=find_packages(),
+)
