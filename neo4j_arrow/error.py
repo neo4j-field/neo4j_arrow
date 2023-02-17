86835327d82cd4a5fa68dd7e41d5d4462a899d8f
Dave Voutila <dave.voutila@neotechnology.com>
2023-02-17 15:10:44 -0500

    forgot to commit the error module

---
+++ neo4j_arrow/error.py
@@ -0,0 +1,67 @@
+"""
+Neo4j Flight Service Errors
+"""
+from pyarrow.lib import ArrowException
+
+
+def interpret(e: ArrowException) -> Exception:
+    """
+    Try to figure out which exception occcurred based on the server response.
+    """
+    try:
+        message = "".join(e.args)
+        if "ALREADY_EXISTS" in message:
+            return AlreadyExists(message)
+        elif "INVALID_ARGUMENT" in message:
+            return InvalidArgument(message)
+        elif "NOT_FOUND" in message:
+            return NotFound(message)
+        elif "INTERNAL" in message:
+            return InternalError(message)
+    except:
+        pass
+    return UnknownError(str(e))
+
+
+class Neo4jArrowException(Exception):
+    """
+    Base class for neo4j_arrow exceptions.
+    """
+    def __init__(self, message: str):
+        self.message = message
+
+
+class UnknownError(Neo4jArrowException):
+    """
+    We have no idea what is wrong :(
+    """
+    pass
+
+
+class AlreadyExists(Neo4jArrowException):
+    """
+    The named graph or database already exists or an import with the name is
+    already running. (Can't distinguish this easily without parsing the message
+    body.)
+    """
+    pass
+
+
+class InvalidArgument(Neo4jArrowException):
+    """
+    Either invalid entity or invalid action requested.
+    """
+    pass
+
+class NotFound(Neo4jArrowException):
+    """
+    The requested import process could not be found.
+    """
+    pass
+
+
+class InternalError(Exception):
+    """
+    Something bad happened on the server side :(
+    """
+    pass
